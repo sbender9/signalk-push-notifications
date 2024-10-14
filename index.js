6430e6b14f84b645de02542ccb0987c94d3e5f50
Scott Bender <scott@scottbender.net>
2024-10-14 11:42:54 -0400

    feature: support sending notifications when switch widgets and controls change on the server chore: updated to latest AWS SDK

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const Bacon = require('baconjs');
 const path = require('path')
 const fs = require('fs')
 const _ = require('lodash')
-const AWS = require('aws-sdk');
+const { SNS } = require('@aws-sdk/client-sns');
 const { createServer, Server, Socket } = require('net')
 const split = require('split')
 
@@ -29,21 +29,42 @@ module.exports = function(app) {
   var server
   var idSequence = 0
   var pushSockets = []
+  var switchStates = {}
   
   plugin.start = function(props) {
     config = props
+    setupSubscriptions()
+    start_local_server()
+  }
+
+  function setupSubscriptions() {
+    unsubscribes.forEach(function(func) { func() })
+    unsubscribes = []
+    
     var command = {
       context: "vessels.self",
       subscribe: [{
         path: "notifications.*",
-        policy: 'instant'
+        period: 1000
       }]
     }
-    
-    app.subscriptionmanager.subscribe(command, unsubscribes, subscription_error, got_delta)
 
-    start_local_server()
-  };
+    let devices = readJson(app, "devices" , plugin.id)
+    Object.values(devices).forEach(device => {
+      if ( device.registeredPaths ) {
+        Object.keys(device.registeredPaths).forEach(path => {
+          command.subscribe.push({
+            path: path,
+            period: 1000
+          })
+        })
+      }
+    })
+
+    app.debug('subscription: ' + JSON.stringify(command))
+
+    app.subscriptionmanager.subscribe(command, unsubscribes, subscription_error, got_delta)
+  }
 
   function subscription_error(err)
   {
@@ -60,7 +81,7 @@ module.exports = function(app) {
   plugin.registerWithRouter = function(router) {
     router.post("/registerDevice", (req, res) => {
 
-      device = req.body
+      let device = req.body
       if ( typeof device.targetArn == 'undefined'
            || typeof device.deviceName == 'undefined'
            || typeof device.accessKey == 'undefined'
@@ -72,14 +93,14 @@ module.exports = function(app) {
         return
       }
       
-      devices = readJson(app, "devices" , plugin.id)
+      let devices = readJson(app, "devices" , plugin.id)
       devices[req.body["targetArn"]] = device
       saveJson(app, "devices", plugin.id, devices, res)
     })
 
     router.post("/deviceEnabled", (req, res) => {
 
-      device = req.body
+      let device = req.body
       if ( typeof device.targetArn == 'undefined' )
       {
         app.debug("invalid request: %O", device)
@@ -88,7 +109,7 @@ module.exports = function(app) {
         return
       }
       
-      devices = readJson(app, "devices" , plugin.id)
+      let devices = readJson(app, "devices" , plugin.id)
       if ( devices[req.body["targetArn"]] == null )
       {
         res.status(404)
@@ -102,7 +123,7 @@ module.exports = function(app) {
 
     router.post("/unregisterDevice", (req, res) => {
 
-      device = req.body
+      let device = req.body
       if ( typeof device.targetArn == 'undefined' )
       {
         app.debug("invalid request:%O ", device)
@@ -123,7 +144,43 @@ module.exports = function(app) {
         delete devices[arn]
         saveJson(app, "devices", plugin.id, devices, res)
       }
-    })    
+    })
+
+    router.post("/registerNotificationPaths", (req, res) => {
+      
+      let targetArn = req.body.targetArn
+      let paths = req.body.paths
+      if ( targetArn ===  undefined
+           || paths === undefined
+         )
+      {
+        app.debug("invalid request: %O", req.body)
+        res.status(400)
+        res.send("Invalid Request")
+        return
+      }
+      
+      let devices = readJson(app, "devices" , plugin.id)
+      let stored = devices[targetArn]
+
+      app.debug(`register paths for ${targetArn} ${JSON.stringify(paths)}`)
+
+      if ( !stored ) {
+        app.debug(`unknown device ${targetArn}`)
+        res.status(404)
+        res.send("Invalid Request")
+      } else {
+        stored.registeredPaths = paths
+        saveJson(app, "devices", plugin.id, devices, res)
+      }
+    })
+
+    router.get("/fake", (req, res) => {
+      _.forIn(devices, function(device, arn) {
+        send_control_push(app, device)
+      })
+      res.send("FAKE!")
+      })
   }
   
   plugin.stop = function() {
@@ -163,11 +220,98 @@ module.exports = function(app) {
     }
   }
 
+  function findRegistrations(path) {
+    let res = {}
+    let devices = readJson(app, "devices" , plugin.id)
+    Object.values(devices).forEach(device => {
+      let controls = []
+      let widgets = [] 
+      
+      let paths = device.registeredPaths
+      if (  paths ) {
+        let pathInfo = paths[path]
+        if ( pathInfo && pathInfo.widgets ) {
+          Object.values(pathInfo.widgets).forEach(widget => {
+            widgets.push(widget)
+          })
+        }
+
+        if ( pathInfo && pathInfo.controls ) {
+          Object.values(pathInfo.controls).forEach(control => {
+            controls.push({
+              token: control.token,
+              kind: control.kind
+            })
+          })
+        }
+        if (controls.length > 0 || widgets.length > 0 ) {
+          res[device.targetArn] = { device, controls, widgets }
+        }
+      }
+    })
+    return res
+  }
+
+  function deviceHasAnyControls(device) {
+    let res = false
+    Object.values(device.registeredPaths).forEach(info => {
+      if (info.controls && info.controls.length > 0 ) {
+        res = true
+      }
+    })
+
+    return res
+  }
+
+  function handleControlChange(vp) {
+    let registrations = findRegistrations(vp.path)
+
+    app.debug(`control changed ${vp.path} = ${vp.value}`)
+    
+    Object.keys(registrations).forEach(targetArn => {
+      let info = registrations[targetArn]
+      let controls = info.controls
+
+      if ( controls.length == 0 && deviceHasAnyControls(info.device) ) {
+        controls = [ { kind:"com.scottbender.Wilhelm.controls.SetSwitch"} ]
+      }
+      controls = controls.map(c => c.kind)
+      
+      app.debug(`sending controls: ${JSON.stringify(controls)} widgets: ${JSON.stringify(info.widgets)} to ${targetArn}`)
+
+      send_backgroup_push(app, info.device, vp.value, vp.value,
+                          controls, info.widgets)
+    })
+
+    /*
+
+    controls.forEach(info => {
+      let isSandbox = info.targetArn.indexOf('APNS_SANDBOX')
+      let path = `/3/device/${info.deviceToken}`
+      let headers = {
+        //':method': 'POST',
+        //':path': path,
+        authorization: `bearer ${info.token}`,
+        "apns-push-type": 'controls',
+        "apns-expiration": 0,
+        "apns-priority": 10,
+        "apns-topic": 'com.scottbender.Wilhelm.push-type.controls',
+      }
+      let post = { aps : { "content-changed" : true } }
+      app.debug(JSON.stringify(headers, 0, 2))
+
+      let host = isSandbox ? 'api.development.push.apple.com' : 'api.push.apple.com'
+      let url = `https://${host}${path}`
+      app.debug(`url: ${url}`)
+      
+    })*/
+  }
 
   function handleNotificationDelta(app, id, notification, last_states)
   {
     //app.debug("notification: %O", notification)
 
+    let devices 
     try {
       devices = readJson(app, "devices", id)
     } catch ( err ) {
@@ -178,35 +322,49 @@ module.exports = function(app) {
     }
 
     notification.updates.forEach(function(update) {
+      if ( update.values === undefined )
+        return
+      
       update.values.forEach(function(value) {
-        if ( value.value != null
-             && typeof value.value.message != 'undefined'
-             && value.value.message != null )
-        {
-          if ( (last_states[value.path] == null
-                && value.value.state != 'normal')
-               || ( last_states[value.path] != null
-                    && last_states[value.path] != value.value.state) )
+        if ( value.path != null
+             && value.path.startsWith('notifications.') ) {
+          if ( value.value != null
+               && typeof value.value.message != 'undefined'
+               && value.value.message != null )
           {
-            last_states[value.path] = value.value.state
-            app.debug("message: %s", value.value.message)
-            if ( typeof config.enableRemotePush === 'undefined'
-                 || config.enableRemotePush )
+            if ( (last_states[value.path] == null
+                  && value.value.state != 'normal')
+                 || ( last_states[value.path] != null
+                      && last_states[value.path] != value.value.state) )
             {
-              _.forIn(devices, function(device, arn) {
-                if ( !deviceIsLocal(device) ) {
-                  send_push(app, device, value.value.message, value.path, value.value.state)
-                } else {
-                  app.debug("Skipping device %s because it's local", device.deviceName)
-                }
-              })
+              last_states[value.path] = value.value.state
+              app.debug("message: %s", value.value.message)
+              if ( typeof config.enableRemotePush === 'undefined'
+                   || config.enableRemotePush )
+              {
+                _.forIn(devices, function(device, arn) {
+                  if ( !deviceIsLocal(device) ) {
+                    send_push(app, device, value.value.message, value.path, value.value.state)
+                  } else {
+                    app.debug("Skipping device %s because it's local", device.deviceName)
+                  }
+                })
+              }
+              send_local_push(value.value.message, value.path, value.value.state)
             }
-            send_local_push(value.value.message, value.path, value.value.state)
           }
-        }
-        else if ( last_states[value.path] )
-        {
-          delete last_states[value.path]
+          else if ( last_states[value.path] )
+          {
+            delete last_states[value.path]
+          }
+        } else {
+          let last = switchStates[value.path]
+          if ( last === undefined || last !== value.value) {
+            switchStates[value.path] = value.value
+            if (last !== undefined ) {
+              handleControlChange(value)
+            }
+          }
         }
       })
     })
@@ -257,6 +415,108 @@ module.exports = function(app) {
                  });
   }
 
+  function send_backgroup_push(app, device, path, value, controls, widgets)
+  {
+    //var aps =   { "aps" : { "content-changed" : 1 } }
+    var aps = {
+      "aps": { "content-available": 1 },
+      "pathUpdate": "path",
+      "value": true,
+      controls,
+      widgets
+    }
+
+    var sns = new SNS({
+      region: "us-east-1",
+
+      credentials: {
+        accessKeyId: device.accessKey,
+        secretAccessKey: device.secretAccessKey
+      }
+    });
+
+    var payload = {
+      default: "path changed",
+      APNS: aps,
+      APNS_SANDBOX: aps
+    };
+
+    // first have to stringify the inner APNS object...
+    payload.APNS = JSON.stringify(payload.APNS);
+    payload.APNS_SANDBOX = JSON.stringify(payload.APNS_SANDBOX);
+
+    app.debug('sending background push to ' + device.targetArn + "payload: " + JSON.stringify(payload, null, 2));
+    
+    // then have to stringify the entire message payload
+    payload = JSON.stringify(payload);
+    
+    sns.publish({
+      Message: payload,
+      MessageStructure: 'json',
+      MessageAttributes: {
+        "AWS.SNS.MOBILE.APNS.PUSH_TYPE": {"DataType":"String","StringValue":"background"},
+        "AWS.SNS.MOBILE.APNS.PRIORITY":{"DataType":"String","StringValue":"10"},
+        "AWS.SNS.MOBILE.APNS.TOPIC":{"DataType":"String","StringValue":"com.scottbender.Wilhelm"}
+      },
+      TargetArn: device.targetArn
+    }, function(err, data) {
+      if (err) {
+        console.log(err.stack);
+        return;
+      }
+      
+      app.debug('push sent');
+    });
+  }
+
+
+  function send_control_push(app, device)
+  {
+    var aps =   { "aps" : { "content-changed" : 1 } }
+
+    var sns = new SNS({
+      region: "us-east-1",
+
+      credentials: {
+        accessKeyId: device.accessKey,
+        secretAccessKey: device.secretAccessKey
+      }
+    });
+
+    var payload = {
+      default: "control changed",
+      APNS: aps,
+      APNS_SANDBOX: aps
+    };
+
+    // first have to stringify the inner APNS object...
+    payload.APNS = JSON.stringify(payload.APNS);
+    payload.APNS_SANDBOX = JSON.stringify(payload.APNS_SANDBOX);
+
+    app.debug('sending push to ' + device.targetArn + "payload: " + JSON.stringify(payload, null, 2));
+    
+    // then have to stringify the entire message payload
+    payload = JSON.stringify(payload);
+    
+    sns.publish({
+      Message: payload,
+      MessageStructure: 'json',
+      MessageAttributes: {
+        "AWS.SNS.MOBILE.APNS.PUSH_TYPE": {"DataType":"String","StringValue":"controls"},
+        "AWS.SNS.MOBILE.APNS.PRIORITY":{"DataType":"String","StringValue":"10"},
+        "AWS.SNS.MOBILE.APNS.TOPIC":{"DataType":"String","StringValue":"com.scottbender.Wilhelm.push-type.controls"}
+      },
+      TargetArn: device.targetArn
+    }, function(err, data) {
+      if (err) {
+        console.log(err.stack);
+        return;
+      }
+      
+      app.debug('push sent');
+    });
+  }
+
   function get_apns(message, path, state)
   {
     if ( message.startsWith('Unknown Seatalk Alarm') ) {
@@ -301,16 +561,20 @@ module.exports = function(app) {
 
   function send_push(app, device, message, path, state)
   {
+    return //FIXME, remove
     var aps = get_apns(message, path, state)
     
     if ( !aps ) {
       return
     }
 
-    var sns = new AWS.SNS({
+    var sns = new SNS({
       region: "us-east-1",
-      accessKeyId: device.accessKey,
-      secretAccessKey: device.secretAccessKey
+
+      credentials: {
+        accessKeyId: device.accessKey,
+        secretAccessKey: device.secretAccessKey
+      }
     });
 
     var payload = {
